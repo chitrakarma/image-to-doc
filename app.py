561cd029cbe0eee764f5019d3360c0d683f0ed25
Chitransh Vishwakarma <chitrakarma@gmail.com>
2020-05-11 17:59:37 +0530

    Updated the main UI
    Combined the buttons using the input-group

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return "Hello World"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/login')
-def login():
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
     return render_template('index.html.jinja', name="Index")
-
-
-@app.route('/register')
-def register():
-    return "Welcome to the register page"
