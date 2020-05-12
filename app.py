ac5fa5062ec41af71841fbbef393010794199a71
Chitransh Vishwakarma <chitrakarma@gmail.com>
2020-05-12 14:41:56 +0530

    Minor project is almost completed
    The download button has started working, upload button works as well, all that is left is formatting the name of the files being downloaded and uploaded

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-from flask import Flask, render_template, request, jsonify, make_response
+import os
+from flask import Flask, render_template, request, jsonify, make_response, send_file
 import pytesseract as tess
 from PIL import Image
 from docx import Document
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        filesize = request.cookies.get("filesize")
         file = request.files["file"]
-
-        print(f"Filesize: {filesize}")
 
         # Converting image to text
         img = Image.open(file)
         text = tess.image_to_string(img)
 
+        # Saving file
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
         # Creating docx file
         doc = Document()
         para = doc.add_paragraph(text)
-        doc.save(f"docs/{file.filename}.docx")
+        doc.save(f"docs/{filename}.docx")
 
-        res = make_response(jsonify({"messasge": f"{file.filename} uploaded"}))
+        res = make_response(jsonify(
+            {"message": f"{file.filename} uploaded", "filename": f"{file.filename}"}))
         return res
 
     return render_template('index.html', name="Index")
