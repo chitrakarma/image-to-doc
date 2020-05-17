8b90da1c35f581284a90d3231f5d9255824a3318
Chitransh Vishwakarma <chitrakarma@gmail.com>
2020-05-17 14:56:53 +0530

    fixed the download file bug, updated report file
    ## Added
    - Introduction
    - Literature Survey
    - Problem Statement
    - Proposed Work
    - Design & Development
    - Implementation
    - References
    - Borders
    - Page numbers
    - Justified text
    - Line height
    - Paragraph Spacing
    
    ## Removed
    - Download button bug

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask, render_template, request, jsonify, make_response, send_file
+from flask import Flask, render_template, request, jsonify, make_response
 import pytesseract as tess
 from PIL import Image
 from docx import Document
+from flask.helpers import send_from_directory
+from os import abort
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads/'
+DOCS_FOLDER = 'docs/'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOCS_FOLDER'] = DOCS_FOLDER
+
+
+@app.route('/get-file/<file_name>')
+def get_file(file_name):
+    try:
+        return send_from_directory(app.config['DOCS_FOLDER'], filename=file_name, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
 
 
 @app.route('/', methods=['GET', 'POST'])
